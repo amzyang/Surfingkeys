66df9be0b057bbd01b21363385d3a4c493a8ce9c
zouyang <zouyang@ym315i.local>
2026-02-15 09:16:48 +0800

    feat: inject user snippets into PDF viewer and markdown viewer
    Extension pages (chrome-extension:// URLs) are not matched by
    userScripts.register() patterns, so user-defined keybindings from
    ~/.surfingkeys.js never applied on these pages. Use
    chrome.scripting.executeScript with USER_SCRIPT world to inject
    snippets into PDF viewer and markdown viewer tabs on initial load.

--- src/background/start.js
+++ src/background/start.js
@@ -1281,6 +1281,19 @@ function start(browser) {
         }
     }
 
+    function injectSnippetsIntoTab(tabId, snippets) {
+        chrome.scripting.executeScript({
+            target: { tabId: tabId },
+            world: "USER_SCRIPT",
+            func: (snippets, rootUrl) => {
+                import(rootUrl + 'api.js').then((module) => {
+                    module.default(rootUrl, new Function('api', 'settings', snippets));
+                });
+            },
+            args: [snippets, chrome.runtime.getURL("/")]
+        });
+    }
+
     function onFullSettingsRequested(data, callback) {
         data.isMV3 = isMV3;
         data.useNeovim = browser.nvimServer && browser.nvimServer.instance;
@@ -1306,6 +1319,12 @@ function start(browser) {
         pf(message.key, function(data) {
             if (message.key === undefined) {
                 onFullSettingsRequested(data);
+                if (isMV3 && data.showAdvanced && data.snippets && sender.url) {
+                    const extPages = ["/pages/pdf_viewer.html", "/pages/markdown.html"];
+                    if (extPages.some(p => sender.url.startsWith(chrome.runtime.getURL(p)))) {
+                        injectSnippetsIntoTab(sender.tab.id, data.snippets);
+                    }
+                }
             }
 
             _response(message, sendResponse, {
